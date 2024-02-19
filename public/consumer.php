9333991add1e7070e04160d3af845221e279839c
Rodger Bittencourt <rodger.bittencourt@zanthus.com.br>
2024-02-19 16:59:44 -0300

    Initial commit

---
+++ public/consumer.php
@@ -0,0 +1,16 @@
+<?php
+
+$consumer = new \RdKafka\Consumer();
+$consumer->addBrokers("kafka:9092");
+
+$topic = $consumer->newTopic("test");
+
+$topic->consumeStart(0, RD_KAFKA_OFFSET_BEGINNING);
+
+echo "consumer started" . PHP_EOL;
+while (true) {
+    $msg = $topic->consume(0, 1000);
+    if (isset($msg->payload)) {
+        echo $msg->payload . PHP_EOL;
+    }
+}
